2e9b2ea165e35cd70f9e055c0d8891129c984042
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-03-29 23:18:45 +0100

    deleted unused comment in App.js

--- src/App.js
+++ src/App.js
@@ -13,9 +13,8 @@ const App = () => {
                     soundcloud: {
                     options: { show_user: false }
                     }
-                }}            
+                }}      
                 url="https://soundcloud.com/chillhopdotcom/sets/lofihiphop?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
-                // controls={true}
                 height="500px"
                 width="200px"
             />
